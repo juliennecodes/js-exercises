3a76cdd6b7a0cd137961e5ca279be0423a9a9d51
julienne <julienne.codes@gmail.com>
2020-05-29 13:30:09 -0400

    Update 22Fizzbuzz.js
    - separate calculation from looping and logging

--- 22Fizzbuzz.js
+++ 22Fizzbuzz.js
@@ -15,3 +15,29 @@ for (number = 1; number <= 100; number++){
     console.log(number);
   }
 }
+
+//Split the calculation of fizzbuzz from looping and logging
+
+// Calculation of FizzBuzz
+function isDivByThree(number) {
+  return number % 3 === 0;
+}
+
+function isDivByFive(number) {
+  return number % 5 === 0;
+}
+
+//Looping and logging
+for (number = 1; number <= 100; number++){
+  if (isDivByThree(number) && isDivByFive(number)) {
+    console.log("Fizzbuzz");
+  }
+
+  else if (isDivByThree(number)) { console.log("Fizz");}
+
+  else if (isDivByFive(number)) {console.log("Buzz");}
+
+  else {
+    console.log(number);
+  }
+}
