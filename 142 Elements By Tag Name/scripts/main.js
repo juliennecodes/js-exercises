6d84b5b302c226acd95fc38943a165630876f82a
julienne <julienne.codes@gmail.com>
2020-07-22 14:26:29 -0400

    Update main.js

--- 142 Elements By Tag Name/scripts/main.js
+++ 142 Elements By Tag Name/scripts/main.js
@@ -1,9 +1,5 @@
-//Problem
-//because descendants is a global binding, every getDescendants call adds onto it
-
-//Redo with descendants instead of direct children
 function byTagName(node, tagName){
-  let descendants = getDescendants(node);
+  let descendants = getChildren(node);
   let wantedTags = [];
 
   for(let descendant of descendants){
@@ -11,61 +7,41 @@ function byTagName(node, tagName){
       wantedTags.push(descendant);
     }
   }
-
   return wantedTags;
 }
 //------------------------------------------------------------------------------
-//recursion?
-//a way to explore nested structures?
-
-
-function x(node, descendants){
-  if (hasChildren(node)){
-    descendants.concat(Array.from(node.children))// this is an error because it returns an HTML collection, I'm not sure if that is an array or not
-    for (let nodeChild of Array.from(node.children)){
-      x(nodeChild);
-    }
-  }
-
-  else {
-    descendants.push(node);
-  }
-
-}
-
-//why nodes? Can't I think of them as elements?
-//what additional meaning does node bring that element doesn't indicate?
-
 function hasChildren(x){
   return x.children.length > 0;
 }
 
+function getChildren(node){
+  let children = [];
 
-//------------------------------------------------------------------------------
-//how to keep global binding private
-//have a function that returns the array
-//so declare array
-//do stuff with array
-//return array
+  if (hasChildren(node)){
+    let nodeChildren = Array.from(node.children);
+    for(let nodeChild of nodeChildren){
+      children.push(nodeChild);
+      if(hasChildren(nodeChild)){
+        getChildrenOfChildren(children);
+      }
+    }//for
+  }//if has children
 
-function getDescendants(node){
-  let descendantsArray = [];
+  return children;
+}//get getChildren
 
-  //do stuff to fill descendants
-  //result should be elements filling up descendants
-  x(node, descendantsArray);
-
-  return descendantsArray;
+function getChildrenOfChildren(childrenArray){
+  for(let child of childrenArray){
+    let nodeChildren = getChildren(child);
+    for(let child of nodeChildren){
+      childrenArray.push(child);
+    }
+  }
 }
-
-byTagName(document.body, "h1");
-
 //------------------------------------------------------------------------------
-//I don't think the descendantsArray is being passed properly
-//an error shows up
-// main.js:24 Uncaught TypeError: Cannot read property 'concat' of undefined
-//     at x (main.js:24)
-//     at x (main.js:26)
-//     at getDescendants (main.js:56)
-//     at byTagName (main.js:6)
-//     at main.js:61
+let body = document.body;
+byTagName(body, "h1");
+
+byTagName(body, "h1")[0];
+
+byTagName(body, "span");
