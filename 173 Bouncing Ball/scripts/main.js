83840b0e776b3efed0a079f2ef5fefab56e8673c
julienne <julienne.codes@gmail.com>
2020-09-04 21:12:55 -0400

    Bouncing Ball

---
+++ 173 Bouncing Ball/scripts/main.js
@@ -0,0 +1,49 @@
+let canvas = document.querySelector("canvas");
+// let cx = canvas.getContext("2d");
+
+// can you have more than one context object in a canvas?
+
+// function createBox(startX, startY, width, height){
+//   let cx = canvas.getContext("2d");
+//   cx.beginPath();
+//   cx.moveTo(startX, startY);
+//   cx.lineTo(startX, height);
+//   cx.lineTo(width, height);
+//   cx.lineTo(width, startY);
+//   cx.closePath();
+//   cx.stroke();
+//
+// }
+//
+// // createBox(0, 0, 50, 100);
+// createBox(20, 20, 320, 320);
+
+class Box{
+  constructor(startX, startY, width, height){
+    this.startX = startX;
+    this.startY = startY;
+    this.width = width;
+    this.height = height;
+  }
+
+  static create(startX, startY, width, height){
+    return new Box(startX, startY, width, height);
+  }
+}
+
+let box = Box.create(20, 20, 320, 320);
+
+console.log(box);
+
+function displayBox(box){
+  let cx = canvas.getContext("2d");
+  cx.beginPath();
+  cx.moveTo(box.startX, box.startY);
+  cx.lineTo(box.startX, box.height);
+  cx.lineTo(box.width, box.height);
+  cx.lineTo(box.width, box.startY);
+  cx.closePath();
+  cx.stroke();
+}
+
+displayBox(box);
