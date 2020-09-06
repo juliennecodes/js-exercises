a7f79e73c8ab509b1ad8350be231ea4c6b160bbf
julienne <julienne.codes@gmail.com>
2020-09-05 20:22:41 -0400

    added display ball

--- 173 Bouncing Ball/scripts/main.js
+++ 173 Bouncing Ball/scripts/main.js
@@ -1,25 +1,7 @@
 let canvas = document.querySelector("canvas");
-// let cx = canvas.getContext("2d");
-
+let cx = canvas.getContext("2d");
 // can you have more than one context object in a canvas?
 
-// function createBox(startX, startY, width, height){
-//   let cx = canvas.getContext("2d");
-//   cx.beginPath();
-//   cx.moveTo(startX, startY);
-//   cx.lineTo(startX, height);
-//   cx.lineTo(width, height);
-//   cx.lineTo(width, startY);
-//   cx.closePath();
-//   cx.stroke();
-//
-// }
-//
-// // createBox(0, 0, 50, 100);
-// createBox(20, 20, 320, 320);
-
-
-//lesson - I think it's better to use an object so you don't have to keep track of what values you gave to a function call.
 class Box{
   constructor(startX, startY, width, height){
     this.startX = startX;
@@ -38,7 +20,7 @@ let box = Box.create(20, 20, 320, 320);
 console.log(box);
 
 function displayBox(box){
-  let cx = canvas.getContext("2d");
+  // let cx = canvas.getContext("2d");
   cx.beginPath();
   cx.moveTo(box.startX, box.startY);
   cx.lineTo(box.startX, box.height);
@@ -49,3 +31,61 @@ function displayBox(box){
 }
 
 displayBox(box);
+
+class Ball{
+  constructor(centerX, centerY, size){
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.size = size;
+    this.radius = size / 2;
+    this.top = centerY - this.radius;
+    this.bottom = centerY + this.radius;
+    this.left = centerX - this.radius;
+    this.right = centerX + this.radius;
+    this.speedX = 0;
+    this.speedY = 10;
+  }
+
+  static create(positionX, positionY, size){
+    return new Ball(positionX, positionY, size);
+  }
+
+  redraw(positionX, positionY, size){
+    this.remove();
+    // return new Ball(positionX, positionY, this.size);
+    // is *this* still available after it gets removed?
+    return new Ball(positionX, positionY, size);
+  }
+}
+
+// let ballA = Ball.create(50, 320, 50);
+// let ballB = Ball.create(300, 245, 100);
+let ballA = Ball.create(50, 295, 50);
+
+function displayBall(ball){
+  // let cx = canvas.getContext("2d");
+  cx.beginPath();
+  cx.arc(ball.centerX , ball.centerY, ball.radius, 0, 2 * Math.PI);
+  cx.fillStyle = "black";
+  cx.fill();
+}
+
+// displayBall(ballA);
+// displayBall(ballB);
+displayBall(ballA);
+
+//so you'll have two parters for animation
+//one for display
+//one for changing the position?
+
+function moveBall(ball, xMovement, yMovement){
+  let newCenterX = ball.centerX + xMovement;
+  let newCenterY = ball.centerY + yMovement;
+  return Ball.create(newCenterX, newCenterY, ball.size);
+}
+
+let movedBallA = moveBall(ballA, 50, 50);
+displayBall(movedBallA);
+
+//how do you remove previous drawing? so you can display a new one?
+//------------------------------------------------------------------------------
