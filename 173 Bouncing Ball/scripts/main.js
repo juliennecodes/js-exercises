6137e2806404bfca5a81a05e7936e09ee5ed7376
julienne <julienne.codes@gmail.com>
2020-09-05 23:31:14 -0400

    organized code a bit

--- 173 Bouncing Ball/scripts/main.js
+++ 173 Bouncing Ball/scripts/main.js
@@ -1,37 +1,23 @@
 let canvas = document.querySelector("canvas");
 let cx = canvas.getContext("2d");
 
-class Box{
-  constructor(startX, startY, width, height){
+//------------------------------------------------------------------------------
+//OBJECTS
+class Box {
+  constructor(startX, startY, width, height) {
     this.startX = startX;
     this.startY = startY;
     this.width = width;
     this.height = height;
   }
 
-  static create(startX, startY, width, height){
+  static create(startX, startY, width, height) {
     return new Box(startX, startY, width, height);
   }
 }
 
-let boxA = Box.create(20, 20, 320, 320);
-
-console.log(boxA);
-
-function displayBox(box){
-  cx.beginPath();
-  cx.moveTo(box.startX, box.startY);
-  cx.lineTo(box.startX, box.height);
-  cx.lineTo(box.width, box.height);
-  cx.lineTo(box.width, box.startY);
-  cx.closePath();
-  cx.stroke();
-}
-
-displayBox(boxA);
-
-class Ball{
-  constructor(centerX, centerY, size){
+class Ball {
+  constructor(centerX, centerY, size) {
     this.centerX = centerX;
     this.centerY = centerY;
     this.size = size;
@@ -44,42 +30,77 @@ class Ball{
     this.speedY = 10;
   }
 
-  static create(positionX, positionY, size){
+  static create(positionX, positionY, size) {
     return new Ball(positionX, positionY, size);
   }
 }
-
+//------------------------------------------------------------------------------
+let boxA = Box.create(20, 20, 320, 320);
 let ballA = Ball.create(50, 295, 50);
-
-function displayBall(ball){
-  cx.beginPath();
-  cx.arc(ball.centerX , ball.centerY, ball.radius, 0, 2 * Math.PI);
-  cx.fillStyle = "black";
-  cx.fill();
-}
-
-displayBall(ballA);
-
-//so you'll have two parters for animation
-//one for display
-//one for changing the position?
-
-function moveBall(ball, xMovement, yMovement){
-  let newCenterX = ball.centerX + xMovement;
-  let newCenterY = ball.centerY + yMovement;
-  return Ball.create(newCenterX, newCenterY, ball.size);
-}
-
-let movedBallA = moveBall(ballA, 50, -50);
-// displayBall(movedBallA);
-
-//how do you remove previous drawing? so you can display a new one?
-function updateDisplay(box, ball){
+//------------------------------------------------------------------------------
+//DISPLAYS & ANIMATION
+function updateDisplay(box, ball) {
   cx.clearRect(box.startX, box.startY, box.width, box.height);
   displayBox(box);
   displayBall(ball);
 }
 
+function displayBox(box) {
+  cx.beginPath();
+  cx.moveTo(box.startX, box.startY);
+  cx.lineTo(box.startX, box.height);
+  cx.lineTo(box.width, box.height);
+  cx.lineTo(box.width, box.startY);
+  cx.closePath();
+  cx.stroke();
+}
+
+function displayBall(ball) {
+  cx.beginPath();
+  cx.arc(ball.centerX, ball.centerY, ball.radius, 0, 2 * Math.PI);
+  cx.fillStyle = "black";
+  cx.fill();
+}
+
+function moveBall(ball, timePassed) {
+  let xMovement = ball.speedX * timePassed;
+  let yMovement = ball.speedY * timePassed;
+  let newCenterX = ball.centerX + xMovement;
+  let newCenterY = ball.centerY + yMovement;
+  return Ball.create(newCenterX, newCenterY, ball.size);
+}
+
+
+//------------------------------------------------------------------------------
+//starts up the display
+displayBox(boxA);
+displayBall(ballA);
+
+//testing animation
+// let movedBallA = moveBall(ballA);
 // updateDisplay(boxA, movedBallA);
 
 //------------------------------------------------------------------------------
+//so how do you add movement
+//and how is that movement scaled by incorporating time passed?
+
+//------------------------------------------------------------------------------
+//ANIMATION STUFF
+//okay, so let's think things through
+//so requestAnimationFrame
+//performs an animation
+//calls a specified function to update an animation before a repaint
+
+let lastTime = null;
+  function frame(time) {
+    if (lastTime != null) {
+      updateAnimation(Math.min(100, time - lastTime) / 1000);
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+
+  function updateAnimation(step) {
+    // Your code here.
+  }
